4a32468d721340ca751e2678b3e4257132419242
mirwox <fmiranda@gmail.com>
2021-06-11 09:28:21 -0700

    envio

---
+++ README.md
@@ -0,0 +1,30 @@
+# Robótica 2021.1 P2
+
+Robótica 2021 - Prova 2 - AF
+
+# Procedimento
+
+O código deve ser submetido via **Github classroom**
+
+O Blackboard deve ser mantido aberto todo o tempo
+
+
+# Enunciado
+
+
+[Enunciado da prova](enunciado.md)
+
+[Enunciado da prova, só que no Jupyter para o preview do Github ficar bom](enunciado.ipynb)
+
+[Reunião da Prova no MS Teams - será usada só para a abertura e chat para avisos **FIQUE ATENTO*](https://teams.microsoft.com/l/meetup-join/19%3a13b6bb25f37644349f4be06935635f83%40thread.tacv2/1623322843933?context=%7b%22Tid%22%3a%226370a6c0-7b90-4709-bd6e-59c28ede833b%22%2c%22Oid%22%3a%2221f99fa6-8962-4ed4-8e67-b6d467b5d276%22%7d)
+
+**Planilha de atendimento**
+Avise aqui se tiver problemas e ligamos para você. *Não* vamos dar manutenção básica nem explicar teoria hoje.   [https://docs.google.com/spreadsheets/d/1RfsG_UPMJGK9t44yhiFIj29x6rARv4HEJuWYsUANtrg/edit?usp=sharing](https://docs.google.com/spreadsheets/d/1RfsG_UPMJGK9t44yhiFIj29x6rARv4HEJuWYsUANtrg/edit?usp=sharing)
+
+
+**Para entregar, faça commits e pushes regulares**
+
+**Integridade Intelectual**
+
+Se você tiver alguma evidência de fraude/cola cometida nesta prova, [use este serviço de e-mail anônimo](https://www.guerrillamail.com/pt/compose)  para informar ao professor.  Ou [este formulário](https://forms.gle/JPhqjPmuKAHxmvwZ9)
+
---
+++ p2_211/scripts/Q4.py
@@ -0,0 +1,89 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+from __future__ import print_function, division
+
+
+# Sugerimos rodar com:
+#         roslaunch my_simulation caixas.launch
+# Depois para rodar este script
+# rosrun p2_211 Q4.py
+
+import rospy
+import numpy as np
+import cv2
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+from nav_msgs.msg import Odometry
+from tf import transformations
+import math
+
+
+ranges = None
+minv = 0
+maxv = 10
+
+bridge = CvBridge()
+
+x = -1
+y = -1
+contador = 0
+radianos = None
+pula = 10
+
+def recebe_odometria(data):
+    global x
+    global y
+    global contador
+    global radianos
+
+    x = data.pose.pose.position.x
+    y = data.pose.pose.position.y
+
+    quat = data.pose.pose.orientation
+    lista = [quat.x, quat.y, quat.z, quat.w]
+    # Angulos tem os angulos absolutos em graus
+    angulos = np.degrees(transformations.euler_from_quaternion(lista))  
+    # Angulos tem os angulos absolutos em radianos
+    radianos =   transformations.euler_from_quaternion(lista)  
+
+    if contador % pula == 0:
+        print("Posicao (x,y)  ({:.2f} , {:.2f}) + angulo {:.2f}".format(x, y,angulos[2]))
+    contador = contador + 1
+
+def scaneou(dado):
+    global ranges
+    global minv
+    global maxv
+    # A variável ranges vai conter as leituras do laser
+    ranges = np.array(dado.ranges).round(decimals=2)
+    minv = dado.range_min 
+    maxv = dado.range_max
+ 
+# A função a seguir é chamada sempre que chega um novo frame
+def roda_todo_frame(imagem):
+    try:
+        cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
+        cv2.imshow("Camera", cv_image)
+        cv2.waitKey(1)
+    except CvBridgeError as e:
+        print('ex', e)
+
+if __name__=="__main__":
+
+    rospy.init_node("q4")
+
+    topico_imagem = "/camera/image/compressed"
+
+    velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
+    
+    recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+    ref_odometria = rospy.Subscriber("/odom", Odometry, recebe_odometria)
+    recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
+
+    while not rospy.is_shutdown():
+        rospy.sleep(0.1)
+
+
+
